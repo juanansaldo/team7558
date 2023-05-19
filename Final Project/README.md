48185bf9eb83582ee7b179d94ba776d65863bb4d
juanansaldo <131718774+juanansaldo@users.noreply.github.com>
2023-05-18 21:03:39 -0700

    chart changes

---
+++ Final Project/README.md
@@ -0,0 +1,8 @@
+Name of project: Cryptocurrency Information
+Names of team members: Ricardo Alire, Juan Ansaldo, Amancio Ramirez, Cory Greer
+Class: CST205-01_SP23
+Date: 05/18/2023
+How to run program: Run the file 'final.py' using the command 'py final.py' in the terminal.
+Link to GitHub repository: https://github.com/juanansaldo/team7558
+Link to Trello board: https://trello.com/b/JzEFdUBx/team-7558-project
+Future work: N/A
--- Final Project/final.py
+++ Final Project/final.py
@@ -1,3 +1,15 @@
+# Course     : CST 205
+# Title      : Cryptocurrency Information
+# Authors    : Juan Ansaldo, Ricardo Alire, Amancio Ramirez, Cory Greer
+# Date       : 17 May 2023
+# Abstract   : This program creates a flask application that uses the
+#              coingecko api to display cryptocurrency information.
+# Work contr : Juan worked on all templates, final.py, css, and javascript.
+#              Ricardo worked on all templates and final.py.
+#              Amancio worked on chart template and final.py.
+#              Cory worked on info template, partials, nav bar, and final.py.
+# Github link: https://github.com/juanansaldo/team7558
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap4
 from pprint import pprint
@@ -6,13 +18,16 @@ import humanize
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
+# formats values using commas
 def format_number(number):
     return humanize.intword(number, format='%.1f')
 
 app = Flask(__name__)
 bootstrap = Bootstrap4(app)
 
+# root route
 @app.route('/')
 def index():
     endpoint = 'https://api.coingecko.com/api/v3/coins/markets?'
@@ -61,6 +76,7 @@ def index():
 
     return render_template('index.html', data=zip(id_list, rank_list, name_list, price_list, logo_list, symbol_list, dayChange_list, dayVolume_list, marketcap_list))
 
+# details route with specific coin info
 @app.route('/detail/<id>')
 def detail(id):
     endpoint = f'https://api.coingecko.com/api/v3/coins/{id}?'
@@ -110,70 +126,41 @@ def detail(id):
 
 @app.route('/chart/<id>')
 def chart(id):
-    endpoint = f'https://api.coingecko.com/api/v3/coins/{id}'
+    endpoint = f'https://api.coingecko.com/api/v3/coins/{id}/market_chart?'
     payload = {
         'vs_currency': 'usd',
-        'market_data': 'true',
-        'days': '30',
-        'interval': 'daily',
-        'tickers': 'false',
-        'community_data': 'false',
-        'developer_data': 'false',
-        'sparkline': 'false'
+        'days': '1,14,30,max',
+        'interval': 'daily'
     }
-
-    name = id.upper()
-    timestamps = []
-    prices = []
-    data = {}
 
     try:
         r = requests.get(endpoint, params=payload)
         if r.ok:
             data = r.json()
 
-            price = data['market_data']['current_price']['usd']
-            dayChange = data['market_data']['price_change_percentage_24h']
-            dayVolume = data['market_data']['total_volume']['usd']
-            marketcap = data['market_data']['market_cap']['usd']
-            circSupply = data['market_data']['circulating_supply']
-            totalSupply = data['market_data']['total_supply']
-            weekChange = data['market_data']['price_change_percentage_7d']
-            yearChange = data['market_data']['price_change_percentage_1y']
-            low = data['market_data']['low_24h']['usd']
-            high = data['market_data']['high_24h']['usd']
+            prices = data['prices']
+            timestamps = [price[0] for price in prices]
+            values = [price[1] for price in prices]
 
-            price = "${:,.2f}".format(price)
-            dayChange = round(dayChange, 2)
-            dayVolume = format_number(dayVolume)
-            marketcap = format_number(marketcap)
-            circSupply = round(circSupply)
-            totalSupply = round(totalSupply)
-            circSupply = format(circSupply, ',')
-            totalSupply = format(totalSupply, ',')
-            weekChange = round(weekChange, 2)
-            yearChange = round(yearChange, 2)
-            low = "${:,.2f}".format(low)
-            high = "${:,.2f}".format(high)
+            fig = go.Figure(data=go.Scatter(x=timestamps, y=values))
+            fig.update_layout(
+            xaxis_title='Timestamp',
+            yaxis_title='Price (USD)',
+            title='Price Chart'
+            )
+            fig.show()
+            # plt.plot(timestamps, values)
+            # plt.xlabel('Timestamp')
+            # plt.ylabel('Price (USD)')
+            # plt.title('Price Chart')
 
-            timestamps, prices = zip(*data['prices'])
-            timestamps = [ts / 60000 for ts in timestamps]
-
-            plotName = id.capitalize()
-
-            plt.clf()
-            plt.plot(timestamps, prices)
-            plt.title(plotName + "'s Current Price Change Per Minute")
-            plt.tick_params(axis='x', labelbottom=False)
-            plt.xlabel('Time (minutes)')
-            plt.ylabel('Value ($)')
-            plt.savefig(f'static/images/{id}_chart.png')
+            # plt.show()
 
     except Exception as e:
         print(e)
+    return render_template('chart.html', id=id ,timestamps=timestamps, values=values)
 
-    return render_template('chart.html', id=id, name=name, imestamps=timestamps, values=prices, low=low, high=high, weekChange=weekChange, yearChange=yearChange, circSupply=circSupply, totalSupply=totalSupply, price=price, dayChange=dayChange, dayVolume=dayVolume, marketcap=marketcap, data=data)
-
+# route with information about cryptocurrencies
 @app.route('/information')
 def information():
     return render_template('information.html')
